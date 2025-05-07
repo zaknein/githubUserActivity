80fdd1fb3d013011f5d97ddb2ef8d2ca5e00e0fc
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-05-07 13:48:37 -0300

    imports

--- App.java
+++ App.java
@@ -4,9 +4,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class App{
     private final HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
